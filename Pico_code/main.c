45de79316a143b4ff8df65cc8e308ae248b23676
BobtheElf <74628352+BobtheElf@users.noreply.github.com>
2024-11-29 16:43:55 -0700

    Updated the readme to include instructions for executing pico code and added support for random pico id numbers and push buttons for more data points.

--- Pico_code/main.c
+++ Pico_code/main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
+#include "pico/rand.h"
 
 #define POTENTIOMETER_PIN 26  // GPIO pin connected to the potentiometer
+#define BUTTON_PIN 15         // GPIO pin connected to the push button
 #define LED_PIN 25
 #define ARRAY_SIZE 100       // Number of readings to store
 
-int main(){
+// Struct to hold potentiometer and button status
+struct DataPoint {
+    uint16_t potentiometer_value;  // ADC value
+    bool button_pressed;           // Button status
+    bool led_on;                    // LED status
+};
 
-    uint16_t readings[ARRAY_SIZE];
+int main(){
+    // Array to store the data
+    struct DataPoint data[ARRAY_SIZE];
+
+    //Initialize chosen serial port
+    stdio_init_all();
 
     // Initialize the ADC
     adc_init();
@@ -17,12 +29,17 @@ int main(){
     adc_gpio_init(POTENTIOMETER_PIN); // GPIO 26 is ADC0
     adc_select_input(0);             // Use ADC input 0 (GPIO 26)
 
+    // Initialize the button pin
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);        // Enable pull-up resistor
+
     //Initialize LED Pin
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    //Initialize chosen serial port
-    stdio_init_all();
+    //Get a random id value
+    uint32_t picoID = get_rand_32();
 
     //Keep track of the indices that need to be changed
     int loop_var = 0;
@@ -35,8 +52,13 @@ int main(){
         gpio_put(LED_PIN, true);
         sleep_ms(1500);
         gpio_put(LED_PIN, false);
-        readings[loop_var] = adc_read(); // Read the potentiometer value
-        printf("Reading %d: %u\n", loop_var, readings[loop_var]); // Print the readings to the console
+        
+        //Collect data from the Pico
+        data[loop_var].potentiometer_value = adc_read();            // Read potentiometer
+        data[loop_var].button_pressed = gpio_get(BUTTON_PIN) == 0;  // Read button (active low)
+        data[loop_var].led_on = true;
+
+        //Go to the next loop value
         loop_var ++;
         if(loop_var > ARRAY_SIZE){
             loop_var = 0;
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # MinisculeDB
 Advanced Data Systems Implementation Project mimicking principles in TinyDB
+
+## Pico Code
+- This contains code to be copied to be loaded onto all of the picos. To set up my Pico SDK, I followed the Shawn Hymel tutorial page and video listed below.
+    - https://shawnhymel.com/2096/how-to-set-up-raspberry-pi-pico-c-c-toolchain-on-windows-with-vs-code/
+    - https://www.youtube.com/watch?v=B5rQSoOmR5w
+- After creating the toolchain, simply run the command:
+```
+cmake -G "MinGW Makefiles" . -B build
+```
+- Once the documents are created run `make`.
+- Press the bootsel button on the Pico and plug it into the computer. This will put the Pico into Bootloader mode and show as a flash drive on your machine. Copy the .uf2 file into this drive.
+
+## Pi Code
+Pending
+
